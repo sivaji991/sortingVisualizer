a8ba25890879a1460461068742bc85ed7da18db5
Sivaji Avulamanda <sivajisalaar@gmail.com>
2026-04-22 16:46:35 +0530

    nothing changed

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // Replace 'searching-visualizer' with your EXACT GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/sivaji991/sortingVisualizer', 
+  base: 'https://github.com/sivaji991/sortingVisualizer/', 
 })
